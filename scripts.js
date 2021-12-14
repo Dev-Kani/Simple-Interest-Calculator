becd8fd0927662fd938b7a9eca69ab80b6cecd4d
Kanishka Samarathunga <kanishkakcs@gmail.com>
2021-12-14 13:07:40 +0530

    completed

---
+++ scripts.js
@@ -0,0 +1,29 @@
+function compute() {
+
+    let inputAmount = document.getElementById("principal").value;
+    let slider = document.getElementById("rate").value;
+
+    let numOfYears = document.getElementById("years").value;
+    let yearlyInterest = ((inputAmount * slider * numOfYears) / 100 + parseFloat(inputAmount));
+    
+    document.getElementById("amount").innerHTML = inputAmount;
+    document.getElementById("rangeValue").innerHTML = slider;
+    document.getElementById("finalAmount").innerHTML = yearlyInterest;
+
+    var year  = new Date().getFullYear();
+    var month = new Date().getMonth();
+    var day   = new Date().getDate();
+
+    var yearsFromToday  = new Date(year + parseFloat(numOfYears), month, day);
+    var justYear = yearsFromToday.getFullYear();
+
+    document.getElementById("numYears").innerHTML = justYear;
+}
+
+var slider = document.getElementById("rate");
+var output = document.getElementById("interest");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
